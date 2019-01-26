4e3bbd4ca4fbc102b7e788d1468f2de52081e3ee
davous267 <davedesign@email.cz>
2019-01-26 22:56:36 +0100

    Sun Day/Night cycle added

---
+++ Assets/Scripts/SunDayNightController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunDayNightController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotationPerSec;
+
+    private void Update()
+    {
+        transform.Rotate(rotationPerSec * Time.deltaTime);
+    }
+}
