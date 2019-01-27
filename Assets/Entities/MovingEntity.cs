799c132f19d7cf332661abf9dc28e25600abc970
davous267 <davedesign@email.cz>
2019-01-27 22:45:01 +0100

    One null check added

--- Assets/Entities/MovingEntity.cs
+++ Assets/Entities/MovingEntity.cs
@@ -54,6 +54,8 @@ namespace Entities {
         }
 
         public void EnqueueBuild(List<TileEntity> path, GameObject prefab) {
+            if(path == null) { return; }
+
             if (path.Last().standingEntity != null) {
                 Debug.Log("Can't build on occupied tile.");
                 return;
